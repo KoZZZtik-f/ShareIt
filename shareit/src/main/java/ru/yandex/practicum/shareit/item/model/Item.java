204db04ed677d3c1aeb1637077e0e62cd0ec94ec
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-17 12:41:57 +0300

    feat: в Item добавлены все нужные поля и аннотации

--- shareit/src/main/java/ru/yandex/practicum/shareit/item/model/Item.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/item/model/Item.java
@@ -1,4 +1,35 @@
 package ru.yandex.practicum.shareit.item.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "items")
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private boolean available;
+
+    @Column(name = "owner_id")
+    private long ownerId;
+
+    // Конструктор без ID
+    public Item(String name, String description, boolean available, long ownerId) {
+        this.name = name;
+        this.description = description;
+        this.available = available;
+        this.ownerId = ownerId;
+    }
 }

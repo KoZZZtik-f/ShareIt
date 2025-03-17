e9d3a6f43b6971a52328cb0f82657f79cc25649e
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-17 12:42:14 +0300

    feat: создан класс ItemDto

---
+++ shareit/src/main/java/ru/yandex/practicum/shareit/item/dto/ItemDto.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.shareit.item.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ItemDto {
+    private String name;
+    private String description;
+    private boolean available;
+}

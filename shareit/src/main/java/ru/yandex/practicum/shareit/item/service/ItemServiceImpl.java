a5a31ada71c229758eac979edeefa43b16bb2060
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-17 12:44:39 +0300

    feat: интерфейс ItemService реализован в ItemServiceImpl классе

---
+++ shareit/src/main/java/ru/yandex/practicum/shareit/item/service/ItemServiceImpl.java
@@ -0,0 +1,61 @@
+package ru.yandex.practicum.shareit.item.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.shareit.item.repository.ItemRepository;
+import ru.yandex.practicum.shareit.item.dto.ItemDto;
+import ru.yandex.practicum.shareit.item.model.Item;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ItemServiceImpl implements ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Override
+    public void addItem(ItemDto itemDto, long userId) {
+        Item item = new Item(itemDto.getName(), itemDto.getDescription(), itemDto.isAvailable(), userId);
+        itemRepository.save(item);
+    }
+
+    @Override
+    public void editItem(long itemId, ItemDto itemDto, long userId) {
+        Item item = itemRepository.findById(itemId);
+
+        // Проверка, что пользователь является владельцем
+        if (item.getOwnerId() != userId) {
+            throw new IllegalArgumentException("User is not the owner of the item");
+        }
+
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setAvailable(itemDto.isAvailable());
+
+        itemRepository.save(item);
+    }
+
+    @Override
+    public ItemDto getItem(long itemId) {
+        Item item = itemRepository.findById(itemId);
+        return new ItemDto(item.getName(), item.getDescription(), item.isAvailable());
+    }
+
+    @Override
+    public List<ItemDto> getItemsByOwner(long userId) {
+        List<Item> items = itemRepository.findByOwnerId(userId);
+        return items.stream()
+                .map(item -> new ItemDto(item.getName(), item.getDescription(), item.isAvailable()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ItemDto> searchItems(String text) {
+        List<Item> items = itemRepository.findByContainingText(text, text);
+        return items.stream()
+                .map(item -> new ItemDto(item.getName(), item.getDescription(), item.isAvailable()))
+                .collect(Collectors.toList());
+    }
+}

4de6c0fc8094223d1f73ccf585514321a38551d1
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-19 23:56:54 +0300

    feat: добавлен User класс в папку Model

---
+++ shareit/src/main/java/ru/yandex/practicum/shareit/user/model/User.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.shareit.user.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+}

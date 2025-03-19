eb5201fe62938ca0e0e3c9bb229a1177d1ca938b
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-20 00:28:27 +0300

    feat: создан класс исключения AvailabilityException

---
+++ shareit/src/main/java/ru/yandex/practicum/shareit/exception/AvailabilityException.java
@@ -0,0 +1,6 @@
+package ru.yandex.practicum.shareit.exception;
+
+public class AvailabilityException extends RuntimeException {
+    public AvailabilityException(String itemIsNotAvailable) {
+    }
+}

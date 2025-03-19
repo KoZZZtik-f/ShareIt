166f8a6efaaf1405aeb7c3d3acb9ccd55e353a2c
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-20 00:28:43 +0300

    feat: создан класс исключения PermissionDeniedException

---
+++ shareit/src/main/java/ru/yandex/practicum/shareit/exception/PermissionDeniedException.java
@@ -0,0 +1,6 @@
+package ru.yandex.practicum.shareit.exception;
+
+public class PermissionDeniedException extends RuntimeException {
+    public PermissionDeniedException(String ownerCannotBookOwnItem) {
+    }
+}

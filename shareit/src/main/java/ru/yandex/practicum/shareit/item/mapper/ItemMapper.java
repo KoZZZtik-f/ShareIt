b005ca1a72f924ecc244c407c132c34599371ad8
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-21 14:03:48 +0300

    feat: реализован класс ItemMapper

--- shareit/src/main/java/ru/yandex/practicum/shareit/item/mapper/ItemMapper.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,5 +1,26 @@
 package ru.yandex.practicum.shareit.item.mapper;
 
+import ru.yandex.practicum.shareit.item.dto.ItemDto;
+import ru.yandex.practicum.shareit.item.model.Item;
+import ru.yandex.practicum.shareit.user.model.User;
+
 public class ItemMapper {
-    // TODO: Напиши меня, пожалуцста!!!!!!!!!
+
+    public static ItemDto toDto(Item item) {
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable()
+        );
+    }
+
+    public static Item toEntity(ItemDto itemDto, User owner) {
+        return new Item(
+                itemDto.getName(),
+                itemDto.getDescription(),
+                itemDto.getAvailable(),
+                owner
+        );
+    }
 }

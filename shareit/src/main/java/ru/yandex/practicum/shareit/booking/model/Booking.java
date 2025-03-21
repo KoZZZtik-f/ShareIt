448380cd62cb2a9fb6b370c3b6b809db4284d5a2
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-21 13:56:08 +0300

    refactor: в Booking массивные конструкторы заменены на @NoArgsConstructor @AllArgsConstructor

--- shareit/src/main/java/ru/yandex/practicum/shareit/booking/model/Booking.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/booking/model/Booking.java
@@ -4,15 +4,16 @@ import jakarta.persistence.*;
 import lombok.*;
 import ru.yandex.practicum.shareit.item.model.Item;
 import ru.yandex.practicum.shareit.user.model.User;
-import java.time.LocalDateTime;
 
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "bookings")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Booking {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,26 +28,11 @@ public class Booking {
     @JoinColumn(name = "item_id", nullable = false)
     private Item item;
 
+    @ManyToOne
+    @JoinColumn(name = "booker_id", nullable = false)
+    private User booker;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private BookingStatus status = BookingStatus.WAITING;
-
-    public Booking() {
-        System.out.println("New booking created");
-    }
-
-    public Booking(Long id, LocalDateTime start, LocalDateTime end, Item item, User booker, BookingStatus status) {
-        if (start.isAfter(end)) {
-            System.out.println("Error: Start date is after end date");
-        }
-        this.id = id;
-        this.start = start;
-        this.end = end;
-        this.item = item;
-        this.status = status;
-    }
-
-    public void printDetails() {
-        System.out.println("Booking details: " + id + ", " + start + " to " + end);
-    }
 }

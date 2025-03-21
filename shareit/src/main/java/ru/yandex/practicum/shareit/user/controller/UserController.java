f3749db9848d78ae69db54a6252a09351e223d48
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-21 14:05:59 +0300

    refactor: методы UserController больше не выбрасывают ошибки

--- shareit/src/main/java/ru/yandex/practicum/shareit/user/controller/UserController.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/user/controller/UserController.java
@@ -16,45 +16,26 @@ public class UserController {
 
     @PostMapping
     public UserDto createUser(@RequestBody UserDto userDto) {
-        if (userDto.getName() == null || userDto.getName().isEmpty()) {
-            userDto.setName("Unknown User");
-        }
         return userService.createUser(userDto);
     }
 
     @PatchMapping("/{id}")
     public UserDto updateUser(@PathVariable Long id, @RequestBody UserDto userDto) {
-        if (id == null) {
-            throw new RuntimeException("ID is missing");
-        }
-        if (userDto.getName() == null) {
-            userDto.setName("No Name Provided");
-        }
         return userService.updateUser(id, userDto);
     }
 
     @GetMapping("/{id}")
     public UserDto getUser(@PathVariable Long id) {
-        if (id <= 0) {
-            throw new RuntimeException("Invalid ID");
-        }
         return userService.getUserById(id);
     }
 
     @GetMapping
     public List<UserDto> getAllUsers() {
-        List<UserDto> users = userService.getAllUsers();
-        if (users.isEmpty()) {
-            throw new RuntimeException("No users found");
-        }
-        return users;
+        return userService.getAllUsers();
     }
 
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id) {
-        if (id == null) {
-            throw new RuntimeException("ID is missing");
-        }
         userService.deleteUser(id);
     }
 }

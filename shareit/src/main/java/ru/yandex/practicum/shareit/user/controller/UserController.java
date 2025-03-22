bb12e64a1c7c8ac06e4a5edd3ed9303b5c0364c3
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-22 12:39:55 +0300

    fix: в UserController добавлено преобразование в Dto перед отправкой ответа

--- shareit/src/main/java/ru/yandex/practicum/shareit/user/controller/UserController.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/user/controller/UserController.java
@@ -3,9 +3,12 @@ package ru.yandex.practicum.shareit.user.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.shareit.user.dto.UserDto;
+import ru.yandex.practicum.shareit.user.mapper.UserMapper;
+import ru.yandex.practicum.shareit.user.model.User;
 import ru.yandex.practicum.shareit.user.service.UserService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -17,25 +20,32 @@ public class UserController {
     // Создание нового пользователя
     @PostMapping
     public UserDto createUser(@RequestBody UserDto userDto) {
-        return userService.createUser(userDto);
+        User user = UserMapper.toEntity(userDto);
+        User createdUser = userService.createUser(user);
+        return UserMapper.toDto(createdUser);
     }
 
     // Обновление пользователя
     @PatchMapping("/{id}")
     public UserDto updateUser(@PathVariable Long id, @RequestBody UserDto userDto) {
-        return userService.updateUser(id, userDto);
+        User user = UserMapper.toEntity(userDto);
+        User updatedUser = userService.updateUser(id, user);
+        return UserMapper.toDto(updatedUser);
     }
 
     // Получение пользователя по ID
     @GetMapping("/{id}")
     public UserDto getUser(@PathVariable Long id) {
-        return userService.getUserById(id);
+        User user = userService.getUserById(id);
+        return UserMapper.toDto(user);
     }
 
     // Получение списка всех пользователей
     @GetMapping
     public List<UserDto> getAllUsers() {
-        return userService.getAllUsers();
+        return userService.getAllUsers().stream()
+                .map(UserMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     // Удаление пользователя по ID

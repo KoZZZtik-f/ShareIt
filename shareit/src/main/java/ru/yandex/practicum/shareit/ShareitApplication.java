c4767d794e8dc47d2ef2426d9fddb05353192339
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-22 12:46:31 +0300

    feat: в класс запуска приложения добавлена отчистка базы данных перед каждым запуском с помощью DatabaseCleanupService

--- shareit/src/main/java/ru/yandex/practicum/shareit/ShareitApplication.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/ShareitApplication.java
@@ -2,12 +2,30 @@ package ru.yandex.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import ru.yandex.practicum.shareit.util.DatabaseCleanupService;
 
 @SpringBootApplication
 public class ShareitApplication {
+
+	private final DatabaseCleanupService databaseCleanupService;
+
+	public ShareitApplication(DatabaseCleanupService databaseCleanupService) {
+		this.databaseCleanupService = databaseCleanupService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShareitApplication.class, args);
 	}
 
+	/*
+	это событие, которое Spring генерирует, когда контекст приложения полностью
+	инициализирован и готов к использованию. Это происходит после того, как все
+	бины созданы, зависимости внедрены, и контекст "освежен" (refresh).
+	 */
+	@EventListener(ContextRefreshedEvent.class)
+	public void onContextRefreshed() {
+		databaseCleanupService.cleanupDatabase(); // Очистка базы данных после инициализации контекста
+	}
 }

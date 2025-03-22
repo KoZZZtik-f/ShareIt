003e83ae34665013078140c90a23a4433832e69e
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-22 13:47:59 +0300

    refactor + style: методы BookingController теперь возвращают просто BookingDtoResponse, а не его ResponseEntity, также теперь мы используем enum-класс State. Названия сделаны более логичными

--- shareit/src/main/java/ru/yandex/practicum/shareit/booking/controller/BookingController.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/booking/controller/BookingController.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.shareit.booking.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.shareit.booking.dto.BookingDtoRequest;
 import ru.yandex.practicum.shareit.booking.dto.BookingDtoResponse;
+import ru.yandex.practicum.shareit.booking.model.State;
 import ru.yandex.practicum.shareit.booking.service.BookingService;
 
 import java.util.List;
@@ -17,42 +17,42 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping
-    public ResponseEntity<BookingDtoResponse> create(
-            @RequestBody BookingDtoRequest dto,
+    public BookingDtoResponse createBooking(
+            @RequestBody BookingDtoRequest bookingDtoRequest,
             @RequestHeader("X-Sharer-User-Id") Long userId) {
-        return ResponseEntity.ok(bookingService.createBooking(dto, userId));
+        return bookingService.createBooking(bookingDtoRequest, userId);
     }
 
     @PatchMapping("/{bookingId}")
-    public ResponseEntity<BookingDtoResponse> approve(
+    public BookingDtoResponse approveBooking(
             @PathVariable Long bookingId,
             @RequestParam boolean approved,
             @RequestHeader("X-Sharer-User-Id") Long userId) {
-        return ResponseEntity.ok(bookingService.approveBooking(bookingId, userId, approved));
+        return bookingService.approveBooking(bookingId, userId, approved);
     }
 
     @GetMapping("/{bookingId}")
-    public ResponseEntity<BookingDtoResponse> getById(
+    public BookingDtoResponse getBookingById(
             @PathVariable Long bookingId,
             @RequestHeader("X-Sharer-User-Id") Long userId) {
-        return ResponseEntity.ok(bookingService.getBooking(bookingId, userId));
+        return bookingService.getBooking(bookingId, userId);
     }
 
     @GetMapping
-    public ResponseEntity<List<BookingDtoResponse>> getUserBookings(
-            @RequestParam(defaultValue = "ALL") String state,
+    public List<BookingDtoResponse> getUserBookings(
+            @RequestParam(defaultValue = "ALL") State state,
             @RequestHeader("X-Sharer-User-Id") Long userId,
             @RequestParam(defaultValue = "0") int from,
             @RequestParam(defaultValue = "10") int size) {
-        return ResponseEntity.ok(bookingService.getUserBookings(userId, state, from, size));
+        return bookingService.getUserBookings(userId, state, from, size);
     }
 
     @GetMapping("/owner")
-    public ResponseEntity<List<BookingDtoResponse>> getOwnerBookings(
-            @RequestParam(defaultValue = "ALL") String state,
+    public List<BookingDtoResponse> getOwnerBookings(
+            @RequestParam(defaultValue = "ALL") State state,
             @RequestHeader("X-Sharer-User-Id") Long userId,
             @RequestParam(defaultValue = "0") int from,
             @RequestParam(defaultValue = "10") int size) {
-        return ResponseEntity.ok(bookingService.getOwnerBookings(userId, state, from, size));
+        return bookingService.getOwnerBookings(userId, state, from, size);
     }
 }

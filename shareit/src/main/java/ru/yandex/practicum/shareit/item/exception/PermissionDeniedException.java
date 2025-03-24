d5e8a74d40a86c9542100a3aa885491421ca45b0
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-24 08:39:42 +0300

    refactor: удалены остатки перенесенного класса PermissionDeniedException

--- shareit/src/main/java/ru/yandex/practicum/shareit/item/exception/PermissionDeniedException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.shareit.item.exception;
-
-public class PermissionDeniedException extends RuntimeException {
-    public PermissionDeniedException(String message) {
-        super(message);
-    }
-}

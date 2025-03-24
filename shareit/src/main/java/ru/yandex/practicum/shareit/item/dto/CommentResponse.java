fd67e5bd01e94ec05d95b14467e9c8b4cc39983d
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-24 08:40:55 +0300

    feat: в CommentResponse добавлена аннотация @AllArgsConstructor

--- shareit/src/main/java/ru/yandex/practicum/shareit/item/dto/CommentResponse.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/item/dto/CommentResponse.java
@@ -1,10 +1,12 @@
 package ru.yandex.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class CommentResponse {
     private Long id; // ID комментария
     private String text; // Текст комментария

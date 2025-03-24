2a34497bb2662bd3e7c910631101af74dafc8c53
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-24 08:47:49 +0300

    feat: в ItemMapper классе добавлена работа с комментариями вещи (Comments)

--- shareit/src/main/java/ru/yandex/practicum/shareit/item/mapper/ItemMapper.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/item/mapper/ItemMapper.java
@@ -3,16 +3,25 @@ package ru.yandex.practicum.shareit.item.mapper;
 import ru.yandex.practicum.shareit.item.dto.ItemDto;
 import ru.yandex.practicum.shareit.item.model.Item;
 
+import java.util.stream.Collectors;
+
 public class ItemMapper {
 
     public static ItemDto toDto(Item item) {
-        return new ItemDto(
-                item.getId(),
-                item.getName(),
-                item.getDescription(),
-                item.getAvailable(),
-                item.getOwner().getId()
-        );
+        ItemDto dto = new ItemDto();
+        dto.setId(item.getId());
+        dto.setName(item.getName());
+        dto.setDescription(item.getDescription());
+        dto.setAvailable(item.getAvailable());
+        dto.setOwnerId(item.getOwner().getId());
+
+        if (item.getComments() != null) {
+            dto.setComments(item.getComments().stream()
+                    .map(CommentMapper::toCommentResponse)
+                    .collect(Collectors.toList()));
+        }
+
+        return dto;
     }
 
     public static Item toEntity(ItemDto itemDto) {

6ef406a26a4706838245478d2b093d3ef695b802
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-24 08:50:14 +0300

    feat: в ItemDto добавлено поле отзывов List comments, и добавлена аннотация @NoArgsConstructor

--- shareit/src/main/java/ru/yandex/practicum/shareit/item/dto/ItemDto.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/item/dto/ItemDto.java
@@ -2,13 +2,18 @@ package ru.yandex.practicum.shareit.item.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ItemDto {
     private Long id;
     private String name;
     private String description;
     private Boolean available;
     private Long ownerId;
+    private List<CommentResponse> comments;
 }

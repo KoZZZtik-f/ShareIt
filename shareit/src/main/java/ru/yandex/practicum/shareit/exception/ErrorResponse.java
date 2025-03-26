e3fccc6e882a4c65c636d7b86113d2a4939f31e6
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-26 08:40:35 +0300

    feat: в ErrorResponse добавлены аннотации @JsonProperty

--- shareit/src/main/java/ru/yandex/practicum/shareit/exception/ErrorResponse.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/exception/ErrorResponse.java
@@ -1,10 +1,14 @@
 package ru.yandex.practicum.shareit.exception;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ErrorResponse {
+    @JsonProperty("Error")
     private String error;
+
+    @JsonProperty("Error message")
     private String description;
 
     public ErrorResponse(Throwable e) {

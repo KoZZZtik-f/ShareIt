3dcff826ec5d114ede2336496739ad58cb4e3cb7
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-26 20:26:59 +0300

    feat: в  добавлены методы deleteUserById и existsByEmailAndIdNot

--- shareit/src/main/java/ru/yandex/practicum/shareit/user/repository/UserRepository.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/user/repository/UserRepository.java
@@ -1,10 +1,22 @@
 package ru.yandex.practicum.shareit.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.shareit.user.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    // метод для проверки email с исключением текущего пользователя
+    boolean existsByEmailAndIdNot(String email, Long id);
+
+    // Добавляем кастомный метод
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id = :id")
+    int deleteUserById(@Param("id") Long id);
+
 }

ad9c81490cc2d3b4e4d43c837417fd9e8380ac29
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-27 21:24:19 +0300

    feat: в UserNotFoundException добавлено сообщение по умолчанию и конструктор с userId

--- shareit/src/main/java/ru/yandex/practicum/shareit/user/exception/UserNotFoundException.java
+++ shareit/src/main/java/ru/yandex/practicum/shareit/user/exception/UserNotFoundException.java
@@ -1,7 +1,14 @@
 package ru.yandex.practicum.shareit.user.exception;
 
 public class UserNotFoundException extends RuntimeException {
+
+    private static final String DEFAULT_MESSAGE = "User with id %d not found";
+
     public UserNotFoundException(String message) {
         super(message);
+    }
+
+    public UserNotFoundException(long userId) {
+        super(String.format(DEFAULT_MESSAGE, userId));
     }
 }
